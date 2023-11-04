70e4e13c82a61da6f1696f48a82c64499897dba0
Hiephanu <doanvanhiep230903@gmail.com>
2023-11-04 23:17:06 +0700

    update sql config production

--- src/config/config.js
+++ src/config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username:process.env.DB_USERNAME||"root",
-    password:process.env.DB_PASSWORD|| null,
-    database:process.env.DB_DATABASE|| "social-app",
-    host: process.env.DB_HOST||"127.0.0.1",
+    username:if0_34935562,
+    password:onc9HoK7YW,
+    database:if0_34935562_socialApp,
+    host: sql109.infinityfree.com,
     dialect: "mysql",
   },
 };
